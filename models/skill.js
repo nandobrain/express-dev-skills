37adcd53337de270a1a720bbac960552d36d2db1
nandobrain <prf.arturo@gmail.com>
2023-01-11 16:07:40 -0800

    stumped on title undifined

---
+++ models/skill.js
@@ -0,0 +1,36 @@
+const skills = [
+    {id: 154654, skill: 'Breakdancing', done: false},
+    {id: 156669, skill: 'Solve a Rubics Cube', done: true},
+    {id: 163232, skill: 'Drifting a Car', done: false }
+];
+
+module.exports = {
+
+    getAll,
+    getOne,
+    create,
+    deleteOne
+};
+
+function getAll() {
+    return skills;
+}
+
+function getOne(id) {
+    // URL params are strings convert to a number
+    id = parseInt(id)
+    return skills.find(skill => skill.id === id)
+
+}
+
+function create(skill) {
+    skill.id = Date.now() % 1000000
+    skill.done = false
+    skills.push(skill)
+}
+
+function deleteOne(id) {
+    id = parseInt(id)
+    const idx = skills.findIndex(skill => skill.id == id)
+    skill.splice(idx, 1)
+}
